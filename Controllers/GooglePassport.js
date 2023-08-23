1fc908616cf18415a0e2c3a95226a4c834628920
Your Name <sm920149@gmail.com>
2023-08-23 09:42:07 +0530

    done

--- Controllers/GooglePassport.js
+++ Controllers/GooglePassport.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://xero-x8k9.onrender.com/auth/google/callback',
+    callbackURL: '/auth/google/callback',
     passReqToCallback   : true,
   },
   function(request, accessToken, refreshToken, profile, done) {
